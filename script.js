be215343243e31a55080ca834049b1ef9782f5f9
mokumoku-chan <maho.kumo1997@gmail.com>
2022-05-07 01:45:44 +0900

    Add script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
+const API_KEY = c22440f9ca6f82135d02de4fbbee95d5
+
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function(data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function(data){
+      alert('通信に失敗しました。')
+    });
   });
 });
